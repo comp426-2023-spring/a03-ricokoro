7c52b020d9f85e3563068fb1b3b311a7ffb631bc
ricokoro <77748744+ricokoro@users.noreply.github.com>
2023-05-09 12:08:53 -0400

    1

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist'
-import { rpsls } from 'node-rpsls'
+import { rpsls } from "../lib/rpsls.js";
 
 
 const args = minimist(process.argv.slice(2));
